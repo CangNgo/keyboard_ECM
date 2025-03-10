cb1ecd5d871d5e6a1bd4cf953abad9849edf9d58
CangNgo <cangngoo123@gmail.com>
2025-03-10 21:04:45 +0700

    update

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,13 +1,14 @@
 from .models import User
 from rest_framework import serializers
 from django.contrib.auth.hashers import  make_password
-
+from rest_framework.decorators import action
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password", "email", "phone_number"]
+        fields = ["id", "username", "password", "email"]
         extra_kwargs = {"password": {"write_only": True}}
-    
+        
+    @action(detail=True, methods=["POST"])
     def create(self, validate_data): 
         validate_data["password"] = make_password(validate_data["password"])
         user = User.objects.create(**validate_data)
