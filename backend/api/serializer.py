82177fa06beddb938acf0e7d4e0df23724c2a677
CangNgo <cangngoo123@gmail.com>
2025-03-14 05:43:26 +0700

    upload image

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,11 +1,12 @@
 from .models import *
 from rest_framework import serializers
 from django.contrib.auth.hashers import  make_password
-from rest_framework.decorators import action
+from . import file_uploader
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password", "email"]
+        fields = ["id", "username", "password", "email", "phone_number"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validate_data):
@@ -18,15 +19,6 @@ class AddressSerializer(serializers.ModelSerializer):
         model = Address
         fields = "__all__"
 
-    def create(self, validated_data):   
-        return Address.objects.create(**validated_data)
-
-    def update(self, instance, validated_data): 
-        for attr, value in validated_data.items():  
-            setattr(instance, attr, value)
-        instance.save()  
-        return instance 
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
@@ -38,9 +30,26 @@ class PropertySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(write_only = True)
+    properties = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Property.objects.all()
+    )
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ['id', 'name', 'dimensions_mm', 'weight_in_grams', 'total_quantity','image','category', 'properties']
+
+    def create(self, validated_data):
+        image = validated_data.pop('image')
+
+        image_url = file_uploader.upload_image(image)
+        properties_data = validated_data.pop('properties')
+
+        product = Product.objects.create(image_url=image_url, **validated_data)
+        product.properties.set(properties_data)
+
+        return product
+
 
 class VariantSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from . import models
-from rest_framework import generics
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import status,generics, mixins
+from rest_framework import generics, mixins
 from . import serializer
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
 
 # Create your views here.
 class CreateUserView (generics.CreateAPIView):
@@ -29,6 +29,7 @@ class CategoryView (
         if 'pk' in kwargs:
             return self.retrieve(request, *args, **kwargs)
         return self.list(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
@@ -63,51 +64,12 @@ class PropertyView(
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
-    
-# Address 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def createAddress(request):
-    # kiểm tra có nhập thiếu dữ liệu hay không
-    required_field = ["user", "city", "district", "ward"]
-    
-    valid = validata_data(request, required_field)
-    if valid :
-        return valid
-    
-    # kiểm tra user có tồn tại hay không 
-    user_id =request.data.get("user")
-    if not models.User.objects.filter(id = user_id).exists(): 
-        return Response({"Error": "User không tồn tại"}, status=status.HTTP_400_BAD_REQUEST)
-    seria = serializer.AddressSerializer(data=request.data)
-    if seria.is_valid():
-        seria.save()
-        return Response(seria.data, status=status.HTTP_201_CREATED)
-    return Response(seria.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Cập nhật địa chỉ
-@api_view(["PUT"])
-@permission_classes([AllowAny])
-def updateAddress(request, pk): 
-    # Kiểm tra địa chỉ có tồn tại không
-    try:
-        instance = models.Address.objects.get(id=pk)
-    except models.Address.DoesNotExist:
-        return Response({"error": "Không tìm thấy địa chỉ cần cập nhật"}, status=status.HTTP_404_NOT_FOUND)
+class ImageView(
+    generics.CreateAPIView,
+    generics.UpdateAPIView,
+):
+    queryset = models.Product.objects.all()
+    serializer_class = serializer.ProductSerializer
+    permission_classes = [AllowAny]
 
-    # Kiểm tra user có tồn tại không
-    if "user" in request.data and not models.User.objects.filter(id=request.data["user"]).exists():
-        return Response({"error": "Người dùng không tồn tại"}, status=status.HTTP_400_BAD_REQUEST)
-
-    # Cập nhật dữ liệu
-    seria = serializer.AddressSerializer(instance, data=request.data, partial=True) 
-    if seria.is_valid():
-        seria.save()
-        return Response(seria.data, status=status.HTTP_200_OK)
-    
-    return Response(seria.errors, status=status.HTTP_400_BAD_REQUEST) 
-# Kiểm tra nếu dữ liệu chưa nhập đủ 
-def validata_data(request, required_field): 
-    miss_field = [field for field in required_field if field not in request.data]
-    if miss_field : 
-        return Response({"Error": f"Nhập thiếu dữ liệu: {', '.join(miss_field)}"}, status= status.HTTP_400_BAD_REQUEST)
