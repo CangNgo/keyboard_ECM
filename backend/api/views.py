b257be599564d801899ef4665ef9caedf1edcca2
CangNgo <cangngoo123@gmail.com>
2025-03-14 13:27:20 +0700

    update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
-from . import models
 from rest_framework import generics, mixins
-from . import serializer
 from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from rest_framework.parsers import FileUploadParser
-from rest_framework.response import Response
+
+from . import models
+from . import serializer
+
 
 # Create your views here.
 class CreateUserView (generics.CreateAPIView):
